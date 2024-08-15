916784e977598c8149cf26a88c6791a6053625d1
Eduardo <s4vitar0810@gmail.com>
2024-08-14 22:51:52 -0600

    Counter Component

---
+++ vue-project/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>
+  Hello World
+</h1>
+
+<button @click="incrementCounter">Increment {{ counter }}</button>
+<button @click="decrementCounter">Decrement</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'; //Para declarar un estado Reactivo se utlizar ref()
+
+const counter = ref(0);
+
+const incrementCounter = () => {
+  counter.value++;
+}
+
+const decrementCounter = () => {
+  counter.value--;
+}
+</script>
+
+<style scoped>
+
+</style>
