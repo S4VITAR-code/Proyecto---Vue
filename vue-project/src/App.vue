633c233a4dc9dc4831d68dc13974907e9dad6764
Eduardo <s4vitar0810@gmail.com>
2024-08-15 19:13:18 -0600

    List Task

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Counter></Counter>
+  <TaskList></TaskList>
 </template>
 
 <script setup>
 import Counter from './components/Counter.vue';
+import TaskList from './components/TaskList.vue';
 </script>
 
 <style scoped>
---
+++ vue-project/src/components/TaskList.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h2>To-Do List</h2>
+  <input type="text">
+  <ul>
+    <li>Task</li>
+  </ul>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tasks = ref([]);
+const newTask = ref('');
+</script>
+
+<style scoped>
+
+</style>
